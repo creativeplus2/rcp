52ab9f30df2938290f16e307f5b62e51629353a4
creativeplusplus <info@creativeplusplus.com>
2024-08-26 16:58:53 +0700

    update

---
+++ rcpstatic/_astro/hoisted.BNXdhVD4.js
@@ -0,0 +1,1 @@
+import{i as t,h as e}from"./ViewTransitions.astro_astro_type_script_index_0_lang.lq8eJf9i.js";t();e();document.addEventListener("astro:after-swap",t);document.addEventListener("astro:after-swap",e);document.addEventListener("astro:after-swap",()=>window.scrollTo({left:0,top:0,behavior:"instant"}));
